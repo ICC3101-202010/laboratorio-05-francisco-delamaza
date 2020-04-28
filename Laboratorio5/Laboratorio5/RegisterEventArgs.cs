82b2ee3fc77823e2a1d093084ae6a759b58883d8
Francisco De la Maza <fjdelamaza@miuandes.cl>
2020-04-28 12:29:02 -0400

    codigo base

---
+++ Laboratorio5/Laboratorio5/RegisterEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio5
+{
+    class RegisterEventArgs: EventArgs
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string VerificationLink { get; set; }
+    }
+}
+
